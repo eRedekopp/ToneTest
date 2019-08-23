fabbbd5ee900133cd40a00338ac1deb5a106464e
err291 <err291@usask.ca>
2019-08-23 13:51:21 -0600

    Added presets to WavTone constructor, added new piano samples to res/raw

--- app/src/main/java/ca/usask/cs/tonesetandroid/HearingTest/Test/SingleWavConfidenceTest.java
+++ app/src/main/java/ca/usask/cs/tonesetandroid/HearingTest/Test/SingleWavConfidenceTest.java
@@ -8,6 +8,9 @@ import ca.usask.cs.tonesetandroid.HearingTest.Tone.WavTone;
 
 public class SingleWavConfidenceTest extends ConfidenceTest<WavTone> {
 
+                                                    //        F4       C5       B5        G6        A7
+    private static final float[] DEFAULT_WAV_FREQUENCIES = {349.23f, 523.25f, 987.77f, 1567.98f, 3520.0f};
+
     /**
      * Tones to play in sampleTones. Configure this in configureTestTones
      */
@@ -17,9 +20,19 @@ public class SingleWavConfidenceTest extends ConfidenceTest<WavTone> {
         super(results, noiseType);
     }
 
+    /**
+     * @throws IllegalArgumentException If frequencies[] contains a value that does not have a default wav resource
+     * (see WavTone constructors)
+     */
     @Override
-    protected void configureTestTones(int trialsPerTone, int volsPerFreq, float[] frequencies) {
+    protected void configureTestTones(int trialsPerTone, int volsPerFreq, float[] frequencies)
+            throws IllegalArgumentException {
         // todo
+    }
+
+    @Override
+    public void initialize() {
+        this.initialize(DEFAULT_TRIALS_PER_TONE, DEFAULT_VOLS_PER_FREQ, DEFAULT_WAV_FREQUENCIES);
     }
 
     @Override
--- app/src/main/java/ca/usask/cs/tonesetandroid/HearingTest/Tone/WavTone.java
+++ app/src/main/java/ca/usask/cs/tonesetandroid/HearingTest/Tone/WavTone.java
@@ -1,5 +1,7 @@
 package ca.usask.cs.tonesetandroid.HearingTest.Tone;
 
+import ca.usask.cs.tonesetandroid.R;
+
 /**
  * A class for a tone whose audio data is stored in a .wav file
  */
@@ -16,6 +18,38 @@ public class WavTone extends ReducibleTone {
 
     public WavTone(int wavResourceID, float freq, double vol) {
         this.wavID = wavResourceID;
+        this.freq = freq;
+        this.vol = vol;
+    }
+
+    /**
+     * Pre-set constructor that automatically selects a resource ID for the given frequency, provided that there is a
+     * default wav ID for that frequency
+     *
+     * @param freq The frequency of the WavTone (Must be one of 349.23, 523.25, 987.77, 1567.98, 3520.00)
+     * @param vol The volume of the WavTone
+     * @throws IllegalArgumentException If freq does not have a default wav resource associated with it
+     */
+    public WavTone(float freq, double vol) throws IllegalArgumentException {
+        switch ((int) freq) {
+            case 349:  // F4 ~= 349 Hz
+                this.wavID = R.raw.f4piano;
+                break;
+            case 523:  // C5 ~= 523 Hz
+                this.wavID = R.raw.c5piano;
+                break;
+            case 987:  // B5 ~= 987 Hz
+                this.wavID = R.raw.b5piano;
+                break;
+            case 1567: // G6 ~= 1.567 kHz
+                this.wavID = R.raw.g6piano;
+                break;
+            case 3520: // A7 ~= 3.52 kHz
+                this.wavID = R.raw.a7piano;
+                break;
+            default:
+                throw new IllegalArgumentException("No default resource id for frequency: " + freq);
+        }
         this.freq = freq;
         this.vol = vol;
     }
